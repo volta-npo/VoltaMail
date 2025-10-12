22b1d533e358e2d0045736593d6aa9eaa6c7d318
sushaan-k <sushaankandukoori@gmail.com>
2025-10-11 21:51:00 -0400

    test(api): simple hello

--- apps/api/api/index.js
+++ apps/api/api/index.js
@@ -1,16 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = async function handler(req, res) {
-  const baseDir = __dirname;
-  const parent = path.join(baseDir, '..');
-  const payload = {
-    baseDir,
-    baseDirEntries: fs.readdirSync(baseDir),
-    parent,
-    parentEntries: fs.readdirSync(parent)
-  };
+module.exports = (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(payload, null, 2));
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
 };
